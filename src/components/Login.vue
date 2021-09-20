b726df65af183c33c111945315f81f4437491f23
349268138 <349268138@qq.com>
2021-09-20 21:59:44 +0800

    add

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,99 +1,29 @@
 <template>
-  <div class="login-form">
-    <div class="g-form">
-      <div class="g-form-line">
-        <span class="g-form-label">用户名：</span>
-        <div class="g-form-input">
-          <input type="text" v-model="userNameModel" placeholder="请输入用户名">
-        </div>
-        <span class="g-form-error">{{ userNameErrors.errorText }}</span>
+  <div>
+    <form>
+      <div class="form-inline">
+        <label for="account">账号</label>
+        <input class="form-control" id="account" placeholder="账号">
       </div>
-    </div>
-    <div class="g-form-line">
-      <span class="g-form-label">密码：</span>
-      <div class="g-form-input">
-        <input type="password" v-model="passwordModel" placeholder="请输入密码">
+      <div class="form-inline">
+        <label for="password">密码</label>
+        <input type="password" class="form-control" id="password" placeholder="密码">
       </div>
-      <span class="g-form-error">{{ passwordErrors.errorText }}</span>
-    </div>
-    <div class="g-form-line">
-      <div class="g-form-btn">
-        <a class="button" @click="login()">登录</a>
-      </div>
-    </div>
-    <p class="g-form-error">{{ errorText }}</p>
+      <button type="submit" class="btn btn-primary">登录</button>
+    </form>
   </div>
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 export default {
   name: 'Login',
   data () {
     return {
-      userNameModel: '',
-      userNameFlag: false,
-      passwordModel: '',
-      passwordFlag: false,
-      errorText: ''
-    }
-  },
-  computed: {
-    userNameErrors () {
-      let errorText, status
-      if (!/^\w{6,20}$/.test(this.userNameModel)) {
-        errorText = '由英文字母、数字或下划线组成且长度大于5小于21'
-        status = false
-      } else {
-        errorText = ''
-        status = true
-      }
-      if (!this.userNameFlag) {
-        errorText = ''
-        this.changeUserNameFlag()
-      }
-      return {
-        errorText,
-        status
-      }
-    },
-    passwordErrors () {
-      let errorText, status
-      if (!/^\w{6,20}$/.test(this.passwordModel)) {
-        errorText = '由英文字母、数字或下划线组成且长度大于5小于21'
-        status = false
-      } else {
-        errorText = ''
-        status = true
-      }
-      if (!this.passwordFlag) {
-        errorText = ''
-        this.changePasswordFlag()
-      }
-      return {
-        errorText,
-        status
-      }
     }
   },
   methods: {
-    login () {
-      if (!this.userNameErrors.status || !this.passwordErrors.status) {
-        this.errorText = '账号或密码校验未通过'
-      } else {
-        this.$http.get('/api/wangjinping/login').then((res) => {
-          this.errorText = ''
-          this.$emit('login-success', res.data)
-        }, (error) => {
-          console.log(error)
-        })
-      }
-    },
-    changeUserNameFlag () {
-      this.userNameFlag = true
-    },
-    changePasswordFlag () {
-      this.passwordFlag = true
-    }
   }
 }
 </script>
