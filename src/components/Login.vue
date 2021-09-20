f1cba3e630269d5b90b11dfcb26a3ed039928855
349268138 <349268138@qq.com>
2021-09-20 22:26:06 +0800

    init bootstrap

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
-    <form>
-      <div class="form-inline">
-        <label for="account">账号</label>
-        <input class="form-control" id="account" placeholder="账号">
+  <form>
+    <div class="form-group">
+      <label for="exampleInputUserName">账号</label>
+      <input type="email" class="form-control" id="exampleInputUserName" placeholder="账号">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">密码</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="密码">
+    </div>
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-primary">登录</button>
       </div>
-      <div class="form-inline">
-        <label for="password">密码</label>
-        <input type="password" class="form-control" id="password" placeholder="密码">
-      </div>
-      <button type="submit" class="btn btn-primary">登录</button>
-    </form>
-  </div>
+    </div>
+  </form>
 </template>
 
 <script>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,129 +1,38 @@
 <template>
-  <div class="login-form">
-    <div class="g-form">
-      <div class="g-form-line">
-        <span class="g-form-label">用户名：</span>
-        <div class="g-form-input">
-          <input type="text" v-model="userNameModel" placeholder="请输入用户名">
-        </div>
-        <span class="g-form-error">{{ userNameErrors.errorText }}</span>
+  <div>
+    <h1>注册组件</h1>
+    <form>
+      <div class="form-inline">
+        <label for="account">账号</label>
+        <input class="form-control" id="account" placeholder="账号">
       </div>
-    </div>
-    <div class="g-form-line">
-      <span class="g-form-label">密码：</span>
-      <div class="g-form-input">
-        <input type="password" v-model="passwordModel" placeholder="请输入密码">
+      <div class="form-inline">
+        <label for="password">密码</label>
+        <input type="password" class="form-control" id="password" placeholder="密码">
       </div>
-      <span class="g-form-error">{{ passwordErrors.errorText }}</span>
-    </div>
-    <div class="g-form-line">
-      <span class="g-form-label">密码确认：</span>
-      <div class="g-form-input">
-        <input type="password" v-model="passwordDoubleCheckModel" placeholder="请输入密码">
+      <div class="form-inline">
+        <label for="password">确认</label>
+        <input type="password" class="form-control" id="password" placeholder="确认密码">
       </div>
-      <span class="g-form-error">{{ passwordDoubleCheckErrors.errorText }}</span>
-    </div>
-    <div class="g-form-line">
-      <div class="g-form-btn">
-        <a class="button" @click="register()">注册</a>
+      <div class="form-inline">
+        <label for="mail">邮箱</label>
+        <input class="form-control" id="mail" placeholder="邮箱">
       </div>
-    </div>
-    <p class="g-form-error">{{ errorText }}</p>
+      <button type="submit" class="btn btn-primary">注册</button>
+    </form>
   </div>
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 export default {
-  name: 'Login',
+  name: 'Register',
   data () {
     return {
-      userNameModel: '',
-      userNameFlag: false,
-      passwordModel: '',
-      passwordFlag: false,
-      passwordDoubleCheckModel: '',
-      passwordDoubleCheckFlag: false,
-      errorText: ''
-    }
-  },
-  computed: {
-    userNameErrors () {
-      let errorText, status
-      if (!/^\w{6,20}$/.test(this.userNameModel)) {
-        errorText = '由英文字母、数字或下划线组成且长度大于5小于21'
-        status = false
-      } else {
-        errorText = ''
-        status = true
-      }
-      if (!this.userNameFlag) {
-        errorText = ''
-        this.changeUserNameFlag()
-      }
-      return {
-        errorText,
-        status
-      }
-    },
-    passwordErrors () {
-      let errorText, status
-      if (!/^\w{6,20}$/.test(this.passwordModel)) {
-        errorText = '由英文字母、数字或下划线组成且长度大于5小于21'
-        status = false
-      } else {
-        errorText = ''
-        status = true
-      }
-      if (!this.passwordFlag) {
-        errorText = ''
-        this.changePasswordFlag()
-      }
-      return {
-        errorText,
-        status
-      }
-    },
-    passwordDoubleCheckErrors () {
-      let errorText, status
-      if (this.passwordDoubleCheckModel !== this.passwordModel) {
-        errorText = '两次密码不同'
-        status = false
-      } else {
-        errorText = ''
-        status = true
-      }
-      if (!this.passwordDoubleCheckFlag) {
-        errorText = ''
-        this.changePasswordDoubleCheckFlag()
-      }
-      return {
-        errorText,
-        status
-      }
     }
   },
   methods: {
-    register () {
-      if (!this.userNameErrors.status || !this.passwordErrors.status || !this.passwordDoubleCheckErrors.status) {
-        this.errorText = '账号或密码校验未通过'
-      } else {
-        this.$http.get('/api/wangjinping/register').then((res) => {
-          this.errorText = ''
-          this.$emit('register-success', res.data)
-        }, (error) => {
-          console.log(error)
-        })
-      }
-    },
-    changeUserNameFlag () {
-      this.userNameFlag = true
-    },
-    changePasswordFlag () {
-      this.passwordFlag = true
-    },
-    changePasswordDoubleCheckFlag () {
-      this.passwordDoubleCheckFlag = true
-    }
   }
 }
 </script>
